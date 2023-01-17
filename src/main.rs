bc1d19a301bdd91570c46d3424d67a43e80ff084
Erdem Harputlugil <47626170+erdh@users.noreply.github.com>
2023-01-18 01:26:46 +0300

    found a better way to count vector

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-use std::{ //using std to get user input
-    io::{
-        self,
-        Write,
-    },
-};
+use std::io::{self};
 use goto::gpoint;
 
 extern crate log; //for logging
 extern crate goto; //for goto
 
 use log::{info, warn}; //using info and warn logging for now
+
 fn main() {
+
     env_logger::init(); //initialise logging
 
     info!("başlıyoruz...");
@@ -19,17 +16,20 @@ fn main() {
 
     println!("- TC No Giriniz: "); //asking user to get tc_no input
 
-    io::stdout().flush().unwrap();
+    let tc_no = get_input().trim().parse::<i64>().unwrap(); //getting input as tc_no with i64
 
-    let tc_no = get_input().trim().parse::<i64>().unwrap();
-    let rakamlar: Vec<_> = tc_no.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect();
+    //let rakamlar: Vec<_> = tc_no.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect(); //dividing tc_no entered
+
     info!("- The input entered by User is {}.",tc_no); //if a number have been entered, the number is logged
 
-        let sayim = rakamlar.iter().count();
+        let digits = x(tc_no as usize);
+
+        let sayim = digits.len(); //counting vector
+
         if sayim == 11 {
             info!("Sayı 11 haneli, devam ediliyor");//if the input has 11 numbers, it is logged and program continues
             let digits = x(tc_no as usize);
-                println!("{:?}", digits);
+            println!("{:?}", digits);
         }
         else {
             warn!("Sayı 11 haneli değil, girilen rakam sayısı {}! İşlem başa alınıyor...",sayim);//if the input doesn't have 11 numbers, it is logged and program starts from 'begin point
@@ -39,6 +39,7 @@ fn main() {
         ];
 
 }
+
 fn x(n: usize) -> Vec<usize> {
     fn x_inner(n: usize, xs: &mut Vec<usize>) {
         if n >= 10 {
@@ -55,3 +56,9 @@ fn get_input() -> String {
     io::stdin().read_line(&mut buffer).expect("Failed");
     buffer
 }
+/*fn sum() {
+    let a = [1, 2, 3, 4, 5];
+    let sum: u8 = a.iter().sum();
+    println!("the total sum is: {}", sum);
+}
+ */
